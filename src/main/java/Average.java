7d747cb6ef2e4d95fcd17d6e1030b9abb757a145
Aaron Alacar <aaron.alacar@gmail.com>
2023-03-08 00:57:31 +0000

    Complete Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,11 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        double avg = (double)total;
+        return avg/nums.length;
     }
 }
